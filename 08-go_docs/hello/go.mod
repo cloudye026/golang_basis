07a90822b02e20c1dc60f6294ce64a867875c28c
cloudye026 <xuguojing77@163.com>
2026-01-21 22:07:30 +0800

    use remote repo mod tidy install local then clean -modcache pkg/mod

--- 08-go_docs/hello/go.mod
+++ 08-go_docs/hello/go.mod
@@ -1,3 +1,5 @@
 module 08-go_docs/hello
 
 go 1.25.5
+
+require github.com/google/go-cmp v0.7.0
--- 08-go_docs/hello/hello.go
+++ 08-go_docs/hello/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"08-go_docs/hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("hello,world")
+
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
